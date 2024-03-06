24f5e44efa84a208a99eb456f841fad09ae35b59
Jonatan Segal <jons@specialisterne.com>
2024-03-06 14:09:23 +0100

    Logger and databasename in credentials

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-from src.credentials import (HOST, USERNAME,PASSWORD,PORT)
-from DBConnector import DBConnection
+from src.credentials import (HOST, USERNAME,PASSWORD,DATABASE,PORT)
+from src.DBConnector import DBConnection
+from src.logger import logger
+
+def main() -> None:
+    try:
+        credentials = {
+            "DB_HOST" : HOST,
+            "DB_USER" : USERNAME,
+            "DB_PASS" : PASSWORD,
+            "DB_NAME" : DATABASE,
+            "DB_PORT" : PORT,
+        }
+        connection = DBConnection.get_instance(credentials)
+        logger.info(f'Connected sucessfully to MySQL database: {connection}')
+        database = DBConnection.create_database(DATABASE)
+        logger.info(f'Database created {database}')
+
+    except TypeError as error:
+        logger.error(f'Error connecting to MySQL database: {error}')    
+
+
+if __name__ == "__main__":
+    main()
