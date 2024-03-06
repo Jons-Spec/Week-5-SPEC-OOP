935fa98579efbcba4abd0771932e588934adbd8e
Jonatan Segal <jons@specialisterne.com>
2024-03-06 15:05:59 +0100

    Database connection now works and creates/checks if database exsists

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.credentials import (HOST, USERNAME,PASSWORD,DATABASE,PORT)
 from src.DBConnector import DBConnection
 from src.logger import logger
+import time
 
 def main() -> None:
     try:
@@ -13,11 +14,20 @@ def main() -> None:
         }
         connection = DBConnection.get_instance(credentials)
         logger.info(f'Connected sucessfully to MySQL database: {connection}')
-        database = DBConnection.create_database(DATABASE)
-        logger.info(f'Database created {database}')
+        database_name = credentials['DB_NAME']
+        # Call create_database method on the DBConnection instance
+        database = DBConnection.create_database(connection, database_name)  # Pass the DBConnection instance as the first argument
+        if database == None:
+            logger.info(f'Database already exists did not create a new one: {database}')
+        else:
+            logger.info(f'Database created: {database}')
+        time.sleep(10)
+        DBConnection.close_instance()
+        logger.info(f'MySQL database closed successfully: {connection}')
 
     except TypeError as error:
-        logger.error(f'Error connecting to MySQL database: {error}')    
+        logger.error(f'Error connecting to MySQL database: {error}')  
+        DBConnection.close_instance()  
 
 
 if __name__ == "__main__":
--- src/DBConnector.py
+++ src/DBConnector.py
@@ -5,12 +5,13 @@ class DBConnection:
     
     __instance = None
 
-    def __init__(self,host: str, username:str, password: str, port:str, database: str=None) -> None:
+    def __init__(self,host: str, username:str, password: str, name: str, port:str) -> None:
         self.host = host
         self.user = username
         self.password = password
+        self.database = name
         self.port = port
-        self.database = database
+    
 
         
         if DBConnection.__instance is None:
@@ -21,7 +22,7 @@ class DBConnection:
             raise Exception("Can't creat another MySQL connection")
         
     def create_database(self, database_name: str) -> None:
-        cursor = DBConnection.__instance,cursor()
+        cursor = DBConnection.__instance.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(database_name))
         cursor.close()
 
@@ -29,7 +30,7 @@ class DBConnection:
     @staticmethod
     def get_instance(credantials: Dict[str,str]) -> object:
         if not DBConnection.__instance:
-            DBConnection(credantials['DB_HOST'], credantials['DB_USER'], credantials['DB_PASS'],credantials['DB_PORT'])
+            DBConnection(credantials['DB_HOST'], credantials['DB_USER'], credantials['DB_PASS'],credantials['DB_NAME'],credantials['DB_PORT'])
         return DBConnection.__instance
     
     @staticmethod
