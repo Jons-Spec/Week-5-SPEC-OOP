f44a73c57d5dc651602c28730f30873d99ba24c8
Jonatan Segal <jons@specialisterne.com>
2024-03-06 13:19:29 +0100

    Checks if DB exsists if not create DB

--- src/DBConnector.py
+++ src/DBConnector.py
@@ -5,21 +5,31 @@ class DBConnection:
     
     __instance = None
 
-    def __init__(self,host: str, username:str, password: str, port:str) -> None:
+    def __init__(self,host: str, username:str, password: str, port:str, database: str=None) -> None:
         self.host = host
         self.user = username
         self.password = password
         self.port = port
+        self.database = database
+
         
         if DBConnection.__instance is None:
             DBConnection.__instance = mysql.connect(host=self.host, username=self.user, password=self.password, port=self.port)
+            if self.database:
+                self.create_database(self.database)
         else:
             raise Exception("Can't creat another MySQL connection")
+        
+    def create_database(self, database_name: str) -> None:
+        cursor = DBConnection.__instance,cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(database_name))
+        cursor.close()
+
         
     @staticmethod
     def get_instance(credantials: Dict[str,str]) -> object:
         if not DBConnection.__instance:
-            DBConnection(credantials['DB_HOST'], credantials['DB_USER'], credantials['DB_PASS'], credantials['DB_PORT'])
+            DBConnection(credantials['DB_HOST'], credantials['DB_USER'], credantials['DB_PASS'],credantials['DB_PORT'])
         return DBConnection.__instance
     
     @staticmethod
